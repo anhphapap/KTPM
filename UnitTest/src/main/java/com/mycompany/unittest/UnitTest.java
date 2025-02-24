2e5ce732d3118c307605a8df8c64191fb2a14048
Your Name <you@example.com>
2025-02-24 13:32:54 +0700

    init

---
+++ UnitTest/src/main/java/com/mycompany/unittest/UnitTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.unittest;
+
+/**
+ *
+ * @author admin
+ */
+public class UnitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
